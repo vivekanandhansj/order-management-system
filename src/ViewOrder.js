dc4936cd95156b38178d9908c2d024689298fc03
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-12-15 21:19:25 +0530

    Update ViewOrder.js

--- src/ViewOrder.js
+++ src/ViewOrder.js
@@ -23,7 +23,7 @@ function Viewrepo() {
     
     useEffect( async () => {
         try {
-            let repoData = await axios.get(`https://oms-server.vercel.app/api/order/viewOrder/${params.id}`, {
+            let repoData = await axios.get(`https://oms-server.onrender.com/api/order/viewOrder/${params.id}`, {
                 headers: {
                   Authorization: window.localStorage.getItem("myapptoken"),
                 },
